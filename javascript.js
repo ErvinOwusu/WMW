2182f4d3a0c5899e8657f5af4ab3b8a51a7d38ea
EC2 Default User <ec2-user@ip-172-31-86-202.ec2.internal>
2022-10-19 12:46:00 +0000

    Added js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// JavaScript File
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener('click', ()=> { 
+    navbarLinks.classList.toggle('active') 
+})
